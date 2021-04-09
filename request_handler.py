4728520b755af2054328237fe25fb827697db8f8
Dan Ross <daniellross@gmail.com>
2021-04-09 10:57:46 -0500

    Added create method for locations

--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from employees import get_all_employees, get_single_employee, create_employee
-from locations import get_all_locations, get_single_location
+from locations import get_all_locations, get_single_location, create_location
 
 
 # Here's a class. It inherits from another class.
@@ -105,6 +105,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         
         if resource == "employees":
             new_item = create_employee(post_body)
+
+        if resource == "locations":
+            new_item = create_location(post_body)
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_item}".encode())
