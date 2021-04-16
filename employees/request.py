b5240f8265253a204e8c7f8f34f9fabdb04da041
Dan Ross <daniellross@gmail.com>
2021-04-16 16:13:00 -0500

    implemented location join on employee request

--- employees/request.py
+++ employees/request.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Employee
+from models import Employee, Location
 
 def get_all_employees():
     with sqlite3.connect("./kennel.db") as conn:
@@ -13,8 +13,12 @@ def get_all_employees():
             e.id,
             e.name,
             e.address,
-            e.location_id
+            e.location_id,
+            l.name location_name,
+            l.address location_address
         FROM employee e
+        JOIN location l
+            ON l.id = e.location_id
         """)
 
         employees = []
@@ -24,6 +28,12 @@ def get_all_employees():
         for row in dataset:
             employee = Employee(row['id'], row['name'], row['address'], 
                                 row['location_id'])
+            
+            location = Location(row['location_id'], row['location_name'],
+                                row['location_address'])
+
+            employee.location = location.__dict__
+
             employees.append(employee.__dict__)
     
     return json.dumps(employees)
